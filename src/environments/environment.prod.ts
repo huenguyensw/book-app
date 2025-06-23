57c9e7ba74cb7d036d42b330d6a2b1cd022eaea2
huenguyensw <hue.blue@gmail.com>
2025-06-23 10:27:32 +0200

    correct apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://bookapi-8cvo.onrender.com/api' // define later 
+  apiUrl: 'https://bookapi-8cvo.onrender.com' // define later 
 };
